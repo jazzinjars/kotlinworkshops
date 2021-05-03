b02b9b8095a20c12f87aca68c2c9c055d60d4073
david.hernando <david.hernando@code.seat>
2021-05-03 10:04:26 +0200

    secondary constructors for mapping

--- src/test/kotlin/code/seat/kotlinworkshopone/MappingObjectsShould.kt
+++ src/test/kotlin/code/seat/kotlinworkshopone/MappingObjectsShould.kt
@@ -4,7 +4,6 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class MappingObjectsShould {
-
     @Test
     internal fun `do not map Objects like this on Kotlin`() {
         val bikeDTO = mapToDTO(BikeEntity("Trek", "road", "Emonda"))
@@ -17,14 +16,25 @@ class MappingObjectsShould {
 
     @Test
     internal fun `map Objects with Expression Functions on Kotlin instead`() {
-        val bikeDTO = BikeEntity("Trek", "road", "Emonda").toDTO()
+        val bike = BikeEntity("Trek", "road", "Emonda")
+        val bikeDTO = bike.toDTO()
         assertThat(bikeDTO.model).isEqualTo("Emonda")
+
+        // also secondary constructor can be used
+        val constructorBikeDTO = BikeDTO(bike)
+        assertThat(constructorBikeDTO.model).isEqualTo("Emonda")
     }
 
     private fun BikeEntity.toDTO() = BikeDTO(brand = brand, type = type, model = model)
 }
 
 data class BikeEntity(val brand: String, val type: String, val model: String)
-data class BikeDTO(val brand: String, val type: String, val model: String)
+data class BikeDTO(val brand: String, val type: String, val model: String) {
+    constructor(bike: BikeEntity): this(
+        brand = bike.brand,
+        type = bike.type,
+        model = bike.model,
+    )
+}
 
 
